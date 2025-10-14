5e6d5e13c131a34a0aa06e1147affc3e65bbe826
salnuraqidah <salnuraqidah@gmail.com>
2025-10-14 07:47:21 +0700

    feat: add hero component to landing page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,3 +1,7 @@
 <x-layout>
     <x-navbar variant="kids" />
+    <x-hero heroTitle="Build the Future, One Code at a Time!"
+        heroSubtitle="Temukan dunia seru penuh imajinasi lewat Coding, AI, dan Robotika! Belajar teknologi kini bisa semenyenangkan bermain."
+        heroCtaText="Daftar Kelas Gratis" heroCtaHref="#contact" :heroImages="[asset('assets/kids/banner.svg')]"
+        stemIcon="{{ asset('assets/kids/stem-icon.svg') }}" googleIcon="{{ asset('assets/kids/google-icon.svg') }}" />
 </x-layout>
