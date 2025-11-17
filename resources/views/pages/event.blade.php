071e083b418b9a825229ce9dc48c84f8010b5c8d
Bagus Adam Farizi <adamfarizi4@gmail.com>
2025-11-17 15:59:18 +0700

    [ADD] menambahkan halaman coming soon dan update halaman not found

--- resources/views/pages/event.blade.php
+++ resources/views/pages/event.blade.php
@@ -1,3 +1,3 @@
 <x-layout title="Event — Alhazen Academy" description="Berbagai Event Alhazen Academy">
-    <x-error.not-found />
+    <x-error.coming-soon launchAt='2025-11-30T00:00:00' />
 </x-layout>
